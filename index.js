3791e705f58b7220687d8a2c257f656cca248785
Ben Anderson <andersjbe@gmail.com>
2024-03-21 11:25:48 -0400

    Completed Solution

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
+const json = require("./api/answers.json");
+
+app.get("/", (req, res) => {
+  res.json(json);
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
